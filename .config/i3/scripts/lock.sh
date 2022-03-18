e86dc82e6eac958b3aa427ab936ffb3f9cd192f1
tibo.depeuter <tibo.depeuter@telenet.be>
2022-03-18 13:01:58 +0100

    Changed lock to darker

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,6 +1,8 @@
 img=/tmp/i3lock.png
+icon=~/.config/i3/scripts/lock-icon-large.png
 
 scrot -z -o $img
-convert $img -scale 20% -scale 500% -gamma 0.8 $img
+convert \( $img -scale 20% -scale 500% -brightness-contrast -30x-30 \) $icon -gravity Center -geometry +0-125 -composite $img
 
-i3lock -i $img #-u
+
+i3lock -i $img -f #-no-unlock-indicator
