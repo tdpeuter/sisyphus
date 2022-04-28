c4ab743d5d99fbcc529c79bf8da24c6b95238a4d
tdpeuter <tibo.depeuter@telenet.be>
2022-04-28 11:27:39 +0200

    Fixed no screen on wake

--- scripts/idle.sh
+++ scripts/idle.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-# Configuratoin of swayidle
+# Configuration of swayidle
 # Just run the script
 
+# Kill previous instances to avoid clashing
 pkill swayidle
 
 swayidle -w \
     timeout 300 \
-        'brightnessctl -s set 15%' \
+        'brightnessctl -s -set 15%' \
         resume 'brightnessctl -r' \
     timeout 600 \
         'swaymsg "output * dpms off"' \
         resume 'swaymsg "output * dpms on"' \
     timeout 1200 \
         'systemctl suspend' \
-    before-sleep 'swaylock -f --screenshots --clock --indicator --effect-blur 5x5'
+    before-sleep 'swaylock -f --screenshots --clock --indicator --effect-blur 5x5; swaymsg "output * dpms on"'
+    # Screen needs to be turned back on or you will get a black screen after waking up again. 
 
