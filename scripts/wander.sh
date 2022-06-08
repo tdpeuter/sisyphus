13556a82d6f319727b8876d71d7d86dbc2f03a96
Tibo De Peuter <tibo.depeuter@telenet.be>
2022-06-08 18:49:23 +0200

    Switched back to brightnessctl, silenced the brightnesswench

--- scripts/wander.sh
+++ scripts/wander.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 # Toggle brightness to 'sleep' or 'awake', since brightnessctl does not support 
 # percentages of current amount.
-# Just run the script (twice)
+# Just run the script
 
-# Permanent memory
-previous=''
-
-if [ -z "${previous}" ] ; then 
-    current=$( cat /sys/class/backlight/intel_backlight/actual_brightness )
-    # Doesn't have to be accurate so we can use built-in calculator.
-    echo $(( current / 10 * 3 )) > /sys/class/backlight/intel_backlight/brightness
-else
-    echo "${previous}" > /sys/class/backlight/intel_backlight/brightness
-fi
-
-# Write to file
-sed -i "s/^previous=.*$/previous=${current:-''}/" "${0}"
+current=$( brightnessctl get )
+# Doesn't have to be accurate so we can use built-in calculator.
+brightnessctl -sq set $(( current / 10 * 3 ))
 
