78b62eae6d37106b315206eb54c93dc67f80deec
Tibo De Peuter <tibo.depeuter@telenet.be>
2022-06-29 17:39:04 +0200

    Updated notification script
    Added support for dunst

--- scripts/dnd.sh
+++ scripts/dnd.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
-# Script to toggle Do not disturb mode for mako
+# Script to toggle Do not disturb mode for mako and dunst
 
 # Permanent memory
 saved_state=0
 
 # Toggle
-if [ "${saved_state}" -eq 0 ] ; then 
+if [[ ${saved_state} -eq 0 ]] ; then 
     ~/.scripts/notify.sh 'Hiding notifications'
-    sleep 2
+    sleep 5
     makoctl set-mode do-not-disturb
+    dunstctl set-paused true
 else 
     makoctl set-mode default
+    dunstctl set-paused false
     ~/.scripts/notify.sh 'Showing notifications'
 fi
 
 # Update status in file
-new_state=$( bc "(${saved_state} + 1) % 2" )
-sed -i "s/saved_state=[0|1]/saved_state=${new_state}/" "${0}"
+new_state=$(( (${saved_state} + 1) % 2 ))
+sed -i "s/^saved_state=.*$/saved_state=${new_state}/" "${0}"
 
