c4e923ffe60d001f895a676159ca46abcadbfde1
Tibo De Peuter <tibo.depeuter@gmail.com>
2022-10-26 20:07:26 +0200

    Added sway and waybar config

---
+++ stow/sway/.config/sway/autostart.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# 
+# ~/.config/sway/autostart.sh
+# List of applications to autostart
+#
+
+function execute () {
+    setsid --fork $SHELL -c "${1}" &> /dev/null
+}
+
+# Idle script
+execute "~/.scripts/idle.sh"
+
+# Gamma and brightness
+execute "clight"
+
+# Notification manager
+execute "dunst -verbosity crit"
+
+# Fix [Slow launch for some GTK apps](https://github.com/swaywm/sway/issues/5732)
+dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+
+# Fix JetBrain IDE's showing properly
+wmname LG3
+
+
+sleep 3
+# --- Everything that requires tray to be active ---
+
+# Nextcloud client
+execute "nextcloud --background"
+
+# Activity watch server & client
+execute "aw-qt"
+
