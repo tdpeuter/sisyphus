817ef563cb86a79e78a76f2d17f4fc666acb7452
tdpeuter <tibo.depeuter@gmail.com>
2023-03-26 22:07:46 +0200

    Add zsh module
    And move script to alias

--- nixos/apply-users.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pushd ~/projects/sisyphus/nixos
-nix build .#homeManagerConfigurations.tdpeuter.activationPackage
-./result/activate
-popd
--- nixos/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pushd ~/projects/sisyphus/nixos/
-nix flake update
-popd
