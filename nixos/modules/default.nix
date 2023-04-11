11712b29303ed592f042ceaf2a4c9bdd25d40ad6
tdpeuter <tibo.depeuter@gmail.com>
2023-04-11 14:53:19 +0200

    Fix Unfree packages not working & styling

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,97 +1,98 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-    imports = [
-        ./apps
-        ./shells
-        ./utils
+  imports = [
+    ./apps
+    ./shells
+    ./utils
+  ];
+  
+  # Nix Flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+    keep-outputs = true
+    keep-derivations = true
+  '';
+
+  users.users.tdpeuter = {
+    description = "Tibo De Peuter";
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    initialPassword = "ChangeMe";
+    packages = with pkgs; [
+      home-manager
     ];
-    
-    # Nix Flakes
-    nix.package = pkgs.nixFlakes;
-    nix.extraOptions = ''
-        experimental-features = nix-command flakes
-        keep-outputs = true
-        keep-derivations = true
-    '';
+    shell = pkgs.zsh;
+  };
 
-    users.users.tdpeuter = {
-        description = "Tibo De Peuter";
-        isNormalUser = true;
-        extraGroups = [ "networkmanager" "wheel" ];
-        initialPassword = "ChangeMe";
-        packages = with pkgs; [
-            home-manager
+  home-manager.useGlobalPkgs = true;
+
+  home-manager.users.tdpeuter = { pkgs, ... }: {
+    home = {
+      username = "tdpeuter";
+      homeDirectory = "/home/tdpeuter";
+      stateVersion = "22.11";
+
+      packages = with pkgs; [
+        brave
+        caprine-bin
+        direnv
+        discord
+        duf
+        git-crypt
+        gnupg
+        jellyfin-media-player
+        libreoffice-fresh-unwrapped
+        lynx
+        mattermost-desktop
+        nextcloud-client
+        obsidian
+        pinentry_qt
+        spotify
+        w3m
+        zathura
+        zenith
+        zoom-us
+        
+        # Fonts
+        corefonts      # Calibri for Uni
+      ];
+    };
+
+    programs = {
+      home-manager.enable = true;
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+
+      git = {
+        enable = true;
+        userName = "tdpeuter";
+        userEmail = "tibo.depeuter@gmail.com";
+        extraConfig = {
+          core.editor = "vim";
+        };
+        includes = [
+          {
+            path = "~/.gitconfig-ugent";
+            condition = "gitdir:~/Nextcloud/Documenten/UGent";
+          }
         ];
-        shell = pkgs.zsh;
+      };
+
+      gpg.enable = true;
+
     };
 
-    home-manager.users.tdpeuter = { pkgs, ... }: {
-        home = {
-            username = "tdpeuter";
-            homeDirectory = "/home/tdpeuter";
-            stateVersion = "22.11";
-          
-            packages = with pkgs; [
-                brave
-                caprine-bin
-                direnv
-                discord
-                duf
-                git-crypt
-                gnupg
-                jellyfin-media-player
-                libreoffice-fresh-unwrapped
-                lynx
-                mattermost-desktop
-                nextcloud-client
-                obsidian
-                pinentry_qt
-                spotify
-                w3m
-                zathura
-                zenith
-                zoom-us
-                
-                # Fonts
-                corefonts      # Calibri for Uni
-            ];
-        };
-        
-        programs = {
-            home-manager.enable = true;
-            
-            direnv = {
-                enable = true;
-                nix-direnv.enable = true;
-            };
-            
-            git = {
-                enable = true;
-                userName = "tdpeuter";
-                userEmail = "tibo.depeuter@gmail.com";
-                extraConfig = {
-                    core.editor = "vim";
-                };
-                includes = [
-                    {
-                        path = "~/.gitconfig-ugent";
-                        condition = "gitdir:~/Nextcloud/Documenten/UGent";
-                    }
-                ];
-            };
-            
-            gpg.enable = true;
-            
-        };
-        
-        services = {
-            gpg-agent = {
-                enable = true;
-                pinentryFlavor = "qt";
-            };
-            
-        };
+    services = {
+      gpg-agent = {
+        enable = true;
+        pinentryFlavor = "qt";
+      };
+
     };
+  };
 
 }
