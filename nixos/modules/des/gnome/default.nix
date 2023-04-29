91cbaf9675cdaddcd9773d4a096a14d3043c9c1d
tdpeuter <tibo.depeuter@gmail.com>
2023-04-29 20:25:20 +0200

    Remove more bloat from Gnome

--- nixos/modules/des/gnome/default.nix
+++ nixos/modules/des/gnome/default.nix
@@ -1,21 +1,39 @@
 { config, lib, pkgs, ... }:
 
 {
-    services.xserver = {
-        enable = true;
+  services.xserver = {
+    enable = true;
+    
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
 
-        # Enable the GNOME Desktop Environment.
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
+    excludePackages = with pkgs; [
+      xterm
+    ];
+  };
 
-    };
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.launch-new-instance
+  ];
 
-    environment.gnome.excludePackages = (with pkgs; [
-        gnome-photos
-        gnome-tour
-        xterm
-    ]) ++ (with pkgs.gnome; [
-        gedit
-        gnome-terminal
-    ]);
+  environment.gnome.excludePackages = (with pkgs; [
+    epiphany # Web browser
+    gnome-console
+    gnome-photos
+    gnome-text-editor
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    geary # Mail client
+    gedit
+    gnome-calendar
+    gnome-clocks
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-weather
+    simple-scan
+    totem # Movie player
+    yelp # Help viewer
+  ]);
 }
