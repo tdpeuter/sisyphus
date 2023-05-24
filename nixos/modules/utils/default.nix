997e2c2586a6ef3389fe5b8782e4c3b61750cd86
tdpeuter <tibo.depeuter@gmail.com>
2023-05-24 21:34:44 +0200

    Split into apps and utils

--- nixos/modules/utils/default.nix
+++ nixos/modules/utils/default.nix
@@ -4,4 +4,37 @@
         ./vim
         ./zellij
     ];
+
+    home-manager.users.tdpeuter = { pkgs, ... }: {
+      home.packages = with pkgs; [
+        direnv
+        duf
+        git-crypt
+        lynx
+        w3m
+        zenith
+      ];
+
+      programs = {
+        direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+
+        git = {
+          enable = true;
+          userName = "tdpeuter";
+          userEmail = "tibo.depeuter@gmail.com";
+          extraConfig = {
+            core.editor = "vim";
+          };
+          includes = [
+            {
+              path = "~/.gitconfig-ugent";
+              condition = "gitdir:~/Nextcloud/Documenten/UGent";
+            }
+          ];
+        };
+      };
+    };
 }
