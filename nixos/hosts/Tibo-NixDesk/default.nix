120945a3134190f450a1d377495766e9cb8ae75f
tdpeuter <tibo.depeuter@gmail.com>
2023-05-24 21:38:30 +0200

    Fix merge conflicts

--- nixos/hosts/Tibo-NixDesk/default.nix
+++ nixos/hosts/Tibo-NixDesk/default.nix
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-=======
->>>>>>> 63c6be3 (Add desktop)
 { config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-<<<<<<< HEAD
-=======
-      ../../modules/hardware/nvidia
->>>>>>> 63c6be3 (Add desktop)
+      ../../modules/hardware/nvidia.nix
 
       ../../modules/apps/virtualbox
       ../../modules/des/gnome
@@ -25,22 +15,10 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-<<<<<<< HEAD
-  networking.hostName = "Tibo-NixDesk"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-=======
   # Enable networking
   networking.hostName = "Tibo-NixDesk"; # Define your hostname.
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
->>>>>>> 63c6be3 (Add desktop)
 
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
@@ -48,28 +26,12 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.utf8";
 
-<<<<<<< HEAD
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-=======
->>>>>>> 63c6be3 (Add desktop)
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
 
-<<<<<<< HEAD
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-=======
->>>>>>> 63c6be3 (Add desktop)
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -87,12 +49,6 @@
     #media-session.enable = true;
   };
 
-<<<<<<< HEAD
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-=======
->>>>>>> 63c6be3 (Add desktop)
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -119,5 +75,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-
 }
