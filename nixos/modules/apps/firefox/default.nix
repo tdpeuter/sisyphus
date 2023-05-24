fea6205877759c9de9d8ddca97f4b48b0d29edcd
tdpeuter <tibo.depeuter@gmail.com>
2023-05-24 21:35:17 +0200

    Fix indents firefox

--- nixos/modules/apps/firefox/default.nix
+++ nixos/modules/apps/firefox/default.nix
@@ -1,41 +1,41 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-    home-manager.users.tdpeuter.programs.firefox = {
-        enable = true;
-        package = pkgs.firefox.override {
-            cfg = {
-                enableTridactylNative = true;
-            };
-            extraPolicies = {
-                DisableFirefoxStudies = true;
-                DisablePocket = true;
-                DisableTelemetry = true;
-                ExtensionSettings = {};
-                OfferToSaveLogins = false;
-            };
-        }; 
-        profiles.tdpeuter = {
-            search = {
-                default = "DuckDuckGo";
-                force = true;
-                engines = {
-                    "eBay".metaData.hidden = true;
-                    "Nix Packages" = {
-                        urls = [{
-                            template = "https://search.nixos.org/packages";
-                            params = [
-                                { name = "type"; value = "packages"; }
-                                { name = "query"; value = "{searchTerms}"; }
-                            ];
-                        }];
-                        
-                        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                        definedAliases = [ "@np" ];
-                    };
-                };
-            };
-        };
+  home-manager.users.tdpeuter.programs.firefox = {
+    enable = true;
+    package = pkgs.firefox.override {
+      cfg = {
+        enableTridactylNative = true;
+      };
+      extraPolicies = {
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        ExtensionSettings = {};
+        OfferToSaveLogins = false;
+      };
     };
+    profiles.tdpeuter = {
+      search = {
+        default = "DuckDuckGo";
+        force = true;
+        engines = {
+          "eBay".metaData.hidden = true;
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+        };
+      };
+    };
+  };
 }
             
