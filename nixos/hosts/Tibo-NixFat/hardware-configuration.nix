1365a9a07bd5e1ea8147e26365f2aa7ad4811778
tdpeuter <tibo.depeuter@gmail.com>
2023-06-14 17:17:06 +0200

    No sleep on lid close, fix slow boot

--- nixos/hosts/Tibo-NixFat/hardware-configuration.nix
+++ nixos/hosts/Tibo-NixFat/hardware-configuration.nix
@@ -4,9 +4,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
@@ -25,9 +25,9 @@
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-label/SWAP"; }
-    ];
+  # swapDevices =
+  #   [ { device = "/dev/disk/by-label/SWAP"; }
+  #   ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
