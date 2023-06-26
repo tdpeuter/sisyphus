050d03ced3aa856239ced93eeb42d6dc90a3641c
tdpeuter <tibo.depeuter@gmail.com>
2023-06-26 17:00:36 +0200

    Tweak vifm handling of video and image

--- nixos/modules/utils/vifm/default.nix
+++ nixos/modules/utils/vifm/default.nix
@@ -1,14 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-    home-manager.users.tdpeuter.home = {
-        packages = with pkgs; [
-            vifm
-            font-awesome_5
-        ];
 
-        file = {
-            ".config/vifm".source = ../../../../stow/vifm/.config/vifm;
-        };
+  home-manager.users.tdpeuter.home = {
+    packages = with pkgs; [
+      vifm
+
+      chafa # Terminal image previewer
+      glow  # Terminal Markdown renderer
+
+      font-awesome_5
+    ];
+
+    # Put files separately so history still works
+    file = {
+      ".config/vifm/colors".source = ../../../../stow/vifm/.config/vifm/colors;
+      ".config/vifm/scripts".source = ../../../../stow/vifm/.config/vifm/scripts;
+      ".config/vifm/vifmrc".source = ../../../../stow/vifm/.config/vifm/vifmrc;
     };
+  };
 }
