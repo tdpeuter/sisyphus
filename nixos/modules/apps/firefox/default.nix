396b5046139659680d7242fd571d88d4a6ed83bc
tdpeuter <tibo.depeuter@gmail.com>
2023-10-07 16:16:33 +0200

    Add speech synthesis

--- nixos/modules/apps/firefox/default.nix
+++ nixos/modules/apps/firefox/default.nix
@@ -1,37 +1,43 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-  home-manager.users.tdpeuter.programs.firefox = {
-    enable = true;
-    package = pkgs.firefox.override {
-      cfg = {
-        enableTridactylNative = true;
-      };
-      extraPolicies = {
-        DisableFirefoxStudies = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        ExtensionSettings = {};
-        OfferToSaveLogins = false;
-      };
-    };
-    profiles.tdpeuter = {
-      search = {
-        default = "DuckDuckGo";
-        force = true;
-        engines = {
-          "eBay".metaData.hidden = true;
-          "Nix Packages" = {
-            urls = [{
-              template = "https://search.nixos.org/packages";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
+  home-manager.users.tdpeuter = {
+    home.packages = with pkgs; [
+      speechd
+    ];
 
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = [ "@np" ];
+    programs.firefox = {
+      enable = true;
+      package = pkgs.firefox.override {
+        cfg = {
+          enableTridactylNative = true;
+        };
+        extraPolicies = {
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          ExtensionSettings = {};
+          OfferToSaveLogins = false;
+        };
+      };
+      profiles.tdpeuter = {
+        search = {
+          default = "DuckDuckGo";
+          force = true;
+          engines = {
+            "eBay".metaData.hidden = true;
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
           };
         };
       };
