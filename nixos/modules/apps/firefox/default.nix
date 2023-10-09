ce2dfee5486ae0275b33e9c4451e259c674255c5
tdpeuter <tibo.depeuter@gmail.com>
2023-10-09 09:17:50 +0200

    [firefox] Add speech synthesis

--- nixos/modules/apps/firefox/default.nix
+++ nixos/modules/apps/firefox/default.nix
@@ -1,43 +1,40 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-  home-manager.users.tdpeuter = {
-    home.packages = with pkgs; [
-      speechd
-    ];
-
-    programs.firefox = {
-      enable = true;
-      package = pkgs.firefox.override {
-        cfg = {
-          enableTridactylNative = true;
-        };
-        extraPolicies = {
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          DisableTelemetry = true;
-          ExtensionSettings = {};
-          OfferToSaveLogins = false;
-        };
+  home-manager.users.tdpeuter.programs.firefox = {
+    enable = true;
+    package = pkgs.firefox.override {
+      cfg = {
+        enableTridactylNative = true;
+        speechSynthesisSupport = true;
       };
-      profiles.tdpeuter = {
-        search = {
-          default = "DuckDuckGo";
-          force = true;
-          engines = {
-            "eBay".metaData.hidden = true;
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
+      extraPolicies = {
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        ExtensionSettings = {};
+        OfferToSaveLogins = false;
+      };
+    };
+    profiles.tdpeuter = {
+      search = {
+        default = "DuckDuckGo";
+        force = true;
+        engines = {
+          "Bing".metaData.hidden = true;
+          "eBay".metaData.hidden = true;
 
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
           };
         };
       };
