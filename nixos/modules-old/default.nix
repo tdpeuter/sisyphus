53dc4ac09f201725e8295e085f910688174b92d7
Tibo De Peuter <tibo.depeuter@gmail.com>
2023-10-17 23:14:25 +0200

    Add home-manager module

--- nixos/modules-old/default.nix
+++ nixos/modules-old/default.nix
@@ -22,20 +22,6 @@
     letter         # Personal font
   ];
 
-  users.users.tdpeuter = {
-    description = "Tibo De Peuter";
-    isNormalUser = true;
-    extraGroups = [ 
-      config.users.groups.keys.name
-      config.users.groups.networkmanager.name
-      config.users.groups.wheel.name
-    ];
-    initialPassword = "ChangeMe";
-    packages = with pkgs; [
-      home-manager
-    ];
-  };
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
   console = {
@@ -47,59 +33,4 @@
     layout = "us";
     xkbVariant = "altgr-intl";
   };
-
-  home-manager.useGlobalPkgs = true;
-
-  home-manager.users.tdpeuter = { pkgs, ... }: {
-    home = {
-      username = "tdpeuter";
-      homeDirectory = "/home/tdpeuter";
-      stateVersion = "23.05";
-
-      packages = with pkgs; [
-        gnupg  
-
-        icosystem
-      ];
-    };
-
-    programs = {
-      home-manager.enable = true;
-      gpg.enable = true;
-    };
-
-    services = {
-      gpg-agent = {
-        enable = true;
-        pinentryFlavor = "qt";
-      };
-
-    };
-
-    xdg.mimeApps =
-      let
-        browser = "firefox.desktop";
-        image-viewer = "nsxiv.desktop";
-      in {
-      enable = true;
-
-      defaultApplications = {
-        "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
-        "application/x-extension-htm" = browser;
-        "application/x-extension-html" = browser;
-        "application/x-extension-shtml" = browser;
-        "application/x-extension-xht" = browser;
-        "application/x-extension-xhtml" = browser;
-        "application/xhtml+xml" = browser;
-        "image/jpeg" = image-viewer;
-        "image/png" = image-viewer;
-        "image/webp" = image-viewer;
-        "text/html" = browser;
-        "x-scheme-handler/chrome" = browser;
-        "x-scheme-handler/http" = browser;
-        "x-scheme-handler/https" = browser;
-      };
-    };
-  };
-
 }
