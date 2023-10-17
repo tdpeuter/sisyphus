a9688167ec2cc9224b0f16626a060d8f8f2c8f93
Tibo De Peuter <tibo.depeuter@gmail.com>
2023-10-17 23:14:45 +0200

    Remove alacritty fake

--- nixos/modules-old/apps/default.nix
+++ nixos/modules-old/apps/default.nix
@@ -1,28 +1,9 @@
 {
     imports = [
-        ./alacritty
         ./firefox
         ./kitty
         ./steam
         ./thunderbird
         # ./virtualbox
     ];
-
-    home-manager.users.tdpeuter = { pkgs, ... }: {
-      home.packages = with pkgs; [
-        brave
-        caprine-bin
-        discord
-        jellyfin-media-player
-        libreoffice-fresh
-        mattermost-desktop
-        nextcloud-client
-        obsidian
-        pinentry_qt
-        qalculate-gtk
-        spotify
-        zathura
-        zoom-us
-      ];
-    };
 }
