b2a29ff790885b4025b23cf0e4ba69cdb8be7dd7
Tibo De Peuter <tibo.depeuter@gmail.com>
2023-10-30 10:30:03 +0100

    [OpenRGB] Fix

--- nixos/modules/services/openrgb/default.nix
+++ nixos/modules/services/openrgb/default.nix
@@ -6,6 +6,14 @@ in {
   options.sisyphus.services.openrgb.enable = lib.mkEnableOption "OpenRGB";
 
   config = lib.mkIf cfg.enable {
+    services.udev.packages = with pkgs; [
+      openrgb
+    ];
+
+    boot.kernelModules = [ "i2c-dev" ];
+
+    hardware.i2c.enable = true;
+
     services.hardware.openrgb = {
       enable = true;
       package = pkgs.openrgb-with-all-plugins;
