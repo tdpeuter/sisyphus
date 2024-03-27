ceda87452c5aca6b79504eaf2dedcc68f734f069
Tibo De Peuter <tibo@depeuter.dev>
2024-03-27 16:29:15 +0100

    Update monero config

--- nixos/modules/services/monero/default.nix
+++ nixos/modules/services/monero/default.nix
@@ -11,22 +11,24 @@ in {
     ];
 
     services = {
+      # Choose to run a node or not.
       monero.enable = true;
+
       xmrig = {
         enable = true;
         settings = {
           autosave = true;
           background = true;
           pause-on-battery = true;
-          pasue-on-active = true;
+          pause-on-active = 60;
           donate-level = 5;
           cpu = true;
           opencl = false;
           cuda = true;
           pools = [
             {
-              # url = "monerohash.com:9999";
-              url = "127.0.0.1:18081"; # Local node
+              url = "monerohash.com:9999";
+              # url = "127.0.0.1:18081"; # Local node
               user = "44FjmmLn1k1GC1AFTLSdWDZ17CHB2h3eRCnfkfTQBucHaKX1AGS5oLERR1FEaHxPQcUNwrbEfsgbY4Y6bYJm6ZrdCYGwg7b";
               keepalive = true;
               tls = true;
