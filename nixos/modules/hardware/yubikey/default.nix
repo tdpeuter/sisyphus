da6d195afbd4d5504cadcf95bc6f0791249deeae
Tibo De Peuter <tibo@depeuter.dev>
2024-04-06 23:00:17 +0200

    [yubikey] Add touch-detector

--- nixos/modules/hardware/yubikey/default.nix
+++ nixos/modules/hardware/yubikey/default.nix
@@ -6,14 +6,31 @@ in {
   options.sisyphus.hardware.yubikey.enable = lib.mkEnableOption "YubiKey support";
 
   config = lib.mkIf cfg.enable {
-    programs.gnupg.agent = {
-      enable = true;
-      # TODO Necessary?
-      # enableSSHSupport = true;
-      # pinentryFlavor = "curses";
+    programs = {
+      gnupg.agent = {
+        enable = true;
+        # TODO Necessary?
+        # enableSSHSupport = true;
+        # pinentryFlavor = "curses";
+      };
+
+      # yubikey-touch-detector.enable = true;
     };
 
     # Enable smart card reading
     services.pcscd.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      yubikey-touch-detector
+    ];
+
+    # Send a notification if the YubiKey is waiting for touch.
+    systemd.user.services.yubikey-touch-detector = {
+      enable = true;
+      description = "Detects when your YubiKey is waiting for a touch";
+      path = with pkgs; [ yubikey-touch-detector ];
+      script = ''exec yubikey-touch-detector --notify'';
+      environment.YUBIKEY_TOUCH_DETECTOR_LIBNOTIFY = "true";
+    };
   };
 }
