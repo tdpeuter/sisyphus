8f021010c328c905634cd9bb30bd3eb8951ce93b
Tibo De Peuter <tibo@depeuter.dev>
2024-04-29 15:03:28 +0200

    [firefox] Remove Qwant from search

--- nixos/users/tdpeuter/firefox.nix
+++ nixos/users/tdpeuter/firefox.nix
@@ -18,42 +18,41 @@ in {
           DisableFirefoxStudies = true;
           DisablePocket = true;
           DisableTelemetry = true;
-          ExtensionSettings = {};
           OfferToSaveLogins = false;
         };
       };
-      profiles.tdpeuter = {
-        search = {
-          default = "DuckDuckGo";
-          force = true;
-          engines = {
-            "Bing".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-  
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-  
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-            "GitHub" = {
-              urls = [{
-                template = "https://github.com/search";
-                params = [
-                  { name = "q"; value = "{searchTerms}"; }
-                  { name = "type"; value = "repositories"; }
-                ];
-              }];
-  
-              icon = "${pkgs.icosystem}/share/icons/icosystem/scalable/apps/github-mark.svg";
-              definedAliases = [ "@gh" ];
-            };
+
+      profiles.tdpeuter.search= {
+        default = "DuckDuckGo";
+        force = true;
+        engines = {
+          "Bing".metaData.hidden = true;
+          "eBay".metaData.hidden = true;
+          "Qwant".metaData.hidden = true;
+
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+          "GitHub" = {
+            urls = [{
+              template = "https://github.com/search";
+              params = [
+                { name = "q"; value = "{searchTerms}"; }
+                { name = "type"; value = "repositories"; }
+              ];
+            }];
+
+            icon = "${pkgs.icosystem}/share/icons/icosystem/scalable/apps/github-mark.svg";
+            definedAliases = [ "@gh" ];
           };
         };
       };
