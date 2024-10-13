0fa8782abbc70d103a91d16edba5dafa44eb942e
Tibo De Peuter <tibo@depeuter.dev>
2024-10-13 17:54:49 +0200

    [virtualbox] Remove custom pkg

--- nixos/modules/virtualisation/virtualbox/default.nix
+++ nixos/modules/virtualisation/virtualbox/default.nix
@@ -2,17 +2,6 @@
 
 let
   cfg = config.sisyphus.virtualisation.virtualbox;
-
-  # I like to override the virtualbox package because it is frequently
-  # and rebuilds take quite long.
-  inherit (pkgs) fetchurl;
-  version = "7.0.10";
-  virtualbox-override = pkgs.virtualbox.overrideAttrs (oldAttrs: {
-    src = fetchurl {
-      url = "https://download.virtualbox.org/virtualbox/${version}/VirtualBox-${version}.tar.bz2";
-      sha256 = "0b1e6d8b7f87d017c7fae37f80586acff04f799ffc1d51e995954d6415dee371";
-    };
-  });
 in {
   options.sisyphus.virtualisation.virtualbox.enable = lib.mkEnableOption "VirtualBox";
 
@@ -21,7 +10,6 @@ in {
       host = {
         enable = true;
         enableExtensionPack = true;
-        package = virtualbox-override;
       };
       guest = {
         enable = true;
