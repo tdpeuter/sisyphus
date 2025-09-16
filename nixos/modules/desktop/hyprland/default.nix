b7b0fa8ad39eef2235c7de59adabd5563897856d
Tibo De Peuter <tibo@depeuter.dev>
2025-09-16 20:59:18 +0200

    chore(hyprland): Update

--- nixos/modules/desktop/hyprland/default.nix
+++ nixos/modules/desktop/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.sisyphus.desktop.hyprland;
@@ -6,9 +6,49 @@ in {
   options.sisyphus.desktop.hyprland.enable = lib.mkEnableOption "Hyprland";
 
   config = lib.mkIf cfg.enable {
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
+    environment = {
+      sessionVariables = {
+        # Hint Electron apps to use wayland
+        NIXOS_OZONE_WL = "1";
+
+        SCRIPT_DIR = ../../../../scripts;
+      };
+
+      systemPackages = with pkgs; [
+        brightnessctl
+        dunst
+        libnotify
+        swaybg
+        waybar
+        waycorner
+        wlsunset
+        wl-clipboard # Copying to system clipboard in vim
+
+        glib
+
+        libva
+
+        dmenu
+        jq
+        j4-dmenu-desktop
+        rofi
+      ];
+    };
+
+    programs = {
+      hyprland = {
+        enable = true;
+        xwayland.enable = true;
+      };
+
+      waybar.enable = true;
+    };
+
+    services = {
+      displayManager.ly.enable = true;
+      gnome.gnome-keyring.enable = true;
+      power-profiles-daemon.enable = true;
+      xserver.videoDrivers = [ "nvidia" ];
     };
   };
 }
