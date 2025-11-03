d24e46da9e8216769e05f99894128fc1a48ca475
Tibo De Peuter <tibo@depeuter.dev>
2025-11-03 19:06:55 +0100

    [openrgb] fix: Also add pkg

--- nixos/modules/services/openrgb/default.nix
+++ nixos/modules/services/openrgb/default.nix
@@ -6,18 +6,24 @@ in {
   options.sisyphus.services.openrgb.enable = lib.mkEnableOption "OpenRGB";
 
   config = lib.mkIf cfg.enable {
-    services.udev.packages = with pkgs; [
-      openrgb
-    ];
-
     boot.kernelModules = [ "i2c-dev" ];
+
+    environment.systemPackages = with pkgs; [
+      openrgb-with-all-plugins
+    ];
 
     hardware.i2c.enable = true;
 
-    services.hardware.openrgb = {
-      enable = true;
-      package = pkgs.openrgb-with-all-plugins;
-      motherboard = "intel";
+    services = {
+      hardware.openrgb = {
+        enable = true;
+        package = pkgs.openrgb-with-all-plugins;
+        motherboard = "intel";
+      };
+
+      udev.packages = with pkgs; [
+        openrgb
+      ];
     };
   };
 }
